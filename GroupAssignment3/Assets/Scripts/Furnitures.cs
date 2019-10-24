10eb7432cd285a76562b6f7a3fd719abc9cab902
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-10-23 22:37:39 -0400

    zhiquan:#1-lock furniture & #2-multipe player controller

--- GroupAssignment3/Assets/Scripts/Furnitures.cs
+++ GroupAssignment3/Assets/Scripts/Furnitures.cs
@@ -2,41 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace GA3
-{
+namespace GA3 {
     [RequireComponent(typeof(Outline))]
     public class Furnitures : MonoBehaviour {
         private Outline outline;
-	    // Use this for initialization
-	    void Start () {
+
+        public bool[] lockScale = new bool[3];
+
+        public bool lockColor = false;
+
+        public List<Color> colors;
+
+        public Color targetColor;
+
+        // Use this for initialization
+        void Start() {
             outline = GetComponent<Outline>();
             outline.enabled = false;
-	    }
-	
-	    // Update is called once per frame
-	    void Update () {
-		
-	    }
-        private void OnMouseDown()
-        {
-            if (MouseController.instance.furniture == null)
-            {
+            //find children colors
+            foreach (Transform child in gameObject.transform) {
+                colors.Add(child.GetComponent<MeshRenderer>().material.color);
+            }
+            targetColor = Color.green;
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+
+        public void Scale(int dir, float s) {
+            if (dir > 2) {
+                return;
+            }
+
+            if (!lockScale[dir]) {
+                lockScale[dir] = true;
+                Vector3 tmpScale = transform.localScale;
+                tmpScale[dir] *= s;
+                transform.localScale = tmpScale;
+            }
+        }
+
+        public void releaseScaleLock(int dir) {
+            if (dir > 2) {
+                return;
+            }
+            lockScale[dir] = false;
+        }
+
+        public void ChangeColor(float ratio) {
+            if (lockColor) {
+                return;
+            }
+            int counter = 0;
+            foreach (Transform child in gameObject.transform) {
+                child.GetComponent<MeshRenderer>().material.color = ratio * (targetColor - colors[counter]) + colors[counter++];
+            }
+        }
+
+        public void colorLock() {
+            lockColor = false;
+        }
+
+        public void ScaleY(float s) {
+        }
+
+        private void OnMouseDown() {
+            if (MouseController.instance.furniture == null) {
                 MouseController.instance.furniture = gameObject;
                 //MouseController.instance.GetOffset();
-            }            
+            }
             else if (MouseController.instance.furniture == gameObject)
                 MouseController.instance.furniture = null;
         }
-        private void OnMouseEnter()
-        {
-            if(MouseController.instance.furniture == null)
+
+        private void OnMouseEnter() {
+            if (MouseController.instance.furniture == null)
                 outline.enabled = true;
         }
-        private void OnMouseExit()
-        {
-            if(MouseController.instance.furniture == null)
+
+        private void OnMouseExit() {
+            if (MouseController.instance.furniture == null)
                 outline.enabled = false;
         }
     }
 }
-
---
+++ GroupAssignment3/Assets/Scripts/KeyboardPlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardPlayerController : MonoBehaviour {
+	public int PlayerID= 0;
+	public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool verticalUpInput = false;
+		bool verticalDownInput = false;
+		bool horizontalLeftInput =false;
+		bool horizontalRightInput = false;
+		if (PlayerID==0) {
+			verticalUpInput = Input.GetKey(KeyCode.W);
+			verticalDownInput = Input.GetKey(KeyCode.S);
+			horizontalLeftInput = Input.GetKey(KeyCode.A);
+			horizontalRightInput = Input.GetKey(KeyCode.D);
+		}else if (PlayerID == 1) {
+			verticalUpInput = Input.GetKey(KeyCode.UpArrow);
+			verticalDownInput = Input.GetKey(KeyCode.DownArrow);
+			horizontalLeftInput = Input.GetKey(KeyCode.LeftArrow);
+			horizontalRightInput = Input.GetKey(KeyCode.RightArrow);
+		}
+		
+		if (verticalUpInput) {
+			this.transform.position += new Vector3(0,0,moveSpeed*Time.deltaTime);
+		}
+
+		if (verticalDownInput) {
+			this.transform.position -= new Vector3(0,0,moveSpeed*Time.deltaTime);
+		}
+		if (horizontalLeftInput) {
+			this.transform.position -= new Vector3(moveSpeed*Time.deltaTime,0,0);
+		}
+		if (horizontalRightInput) {
+			this.transform.position += new Vector3(moveSpeed*Time.deltaTime,0,0);
+		}
+	}
+}
