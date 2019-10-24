7ad085ba35c43b4f16d16b59f81d10bdb85e738e
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-10-24 10:37:50 -0400

    issue#8 & issue#6 completed + tested

--- GroupAssignment3/Assets/Scripts/Furnitures.cs
+++ GroupAssignment3/Assets/Scripts/Furnitures.cs
@@ -7,12 +7,12 @@ namespace GA3 {
     public class Furnitures : MonoBehaviour {
         private Outline outline;
 
-        public bool[] lockScale = new bool[3];
-
-        public bool lockColor = false;
+        public int[] lockScale = new int[3]{-1,-1,-1};
+        public Vector2 scaleRange = new Vector2(0.2f,2);
+        public int lockColor = -1;
 
         public List<Color> colors;
-
+        public float colorRatio = 0;
         public Color targetColor;
 
         // Use this for initialization
@@ -24,47 +24,54 @@ namespace GA3 {
                 colors.Add(child.GetComponent<MeshRenderer>().material.color);
             }
             targetColor = Color.green;
+            //reset locker
+            lockColor = -1;
+            for (int i = 0; i < 3; ++i) {
+                lockScale[i] = -1;
+            }
         }
 
         // Update is called once per frame
         void Update() {
         }
 
-        public void Scale(int dir, float s) {
+        public void Scale(int playerID,int dir, float s) {
             if (dir > 2) {
                 return;
             }
-
-            if (!lockScale[dir]) {
-                lockScale[dir] = true;
+            print("A");
+            if (lockScale[dir] == -1 || lockScale[dir] == playerID) {
+                lockScale[dir] = playerID;
                 Vector3 tmpScale = transform.localScale;
-                tmpScale[dir] *= s;
+                tmpScale[dir] += s;
                 transform.localScale = tmpScale;
             }
         }
 
-        public void releaseScaleLock(int dir) {
-            if (dir > 2) {
-                return;
+        public void ReleaseScaleLock(int playerId) {
+            // release scale locker
+            for (int i = 0; i < lockScale.Length;++i) {
+                if (lockScale[i] == playerId) {
+                    lockScale[i] = -1;
+                }
             }
-            lockScale[dir] = false;
-        }
-
-        public void ChangeColor(float ratio) {
-            if (lockColor) {
-                return;
-            }
-            int counter = 0;
-            foreach (Transform child in gameObject.transform) {
-                child.GetComponent<MeshRenderer>().material.color = ratio * (targetColor - colors[counter]) + colors[counter++];
+            
+            //release color color locker
+            if (lockColor == playerId) {
+                lockColor = -1;
             }
         }
 
-        public void colorLock() {
-            lockColor = false;
-        }
-
-        public void ScaleY(float s) {
+        public void ChangeColor(int playerID,float ratio) {
+            if (lockColor == -1 || lockColor == playerID) {
+                lockColor = playerID;
+                int counter = 0;
+                colorRatio += ratio;
+                foreach (Transform child in gameObject.transform) {
+                    child.GetComponent<MeshRenderer>().material.color =
+                        colorRatio * (targetColor - colors[counter]) + colors[counter++];
+                }
+            }
         }
 
         private void OnMouseDown() {
--- GroupAssignment3/Assets/Scripts/KeyboardPlayerController.cs
+++ GroupAssignment3/Assets/Scripts/KeyboardPlayerController.cs
@@ -1,10 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using GA3;
 using UnityEngine;
 
 public class KeyboardPlayerController : MonoBehaviour {
 	public int PlayerID= 0;
 	public float moveSpeed;
+	public int modeID = 0;//0:Scale X 1:Scale Y 2:Scale Z 3:Color 4:Move
+	public int modeNum = 5;
+
+	public float manipulateStep = 0.1f;
+
+	public GameObject targetFurniture;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,34 +20,112 @@ public class KeyboardPlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		bool verticalUpInput = false;
-		bool verticalDownInput = false;
-		bool horizontalLeftInput =false;
-		bool horizontalRightInput = false;
-		if (PlayerID==0) {
-			verticalUpInput = Input.GetKey(KeyCode.W);
-			verticalDownInput = Input.GetKey(KeyCode.S);
-			horizontalLeftInput = Input.GetKey(KeyCode.A);
-			horizontalRightInput = Input.GetKey(KeyCode.D);
+		MovePlayer();
+		SwitchMode();
+		ManipulateFurniture();
+	}
+
+	void MovePlayer() {
+				bool verticalUpInput = false;
+        		bool verticalDownInput = false;
+        		bool horizontalLeftInput =false;
+        		bool horizontalRightInput = false;
+        		if (PlayerID==0) {
+        			verticalUpInput = Input.GetKey(KeyCode.W);
+        			verticalDownInput = Input.GetKey(KeyCode.S);
+        			horizontalLeftInput = Input.GetKey(KeyCode.A);
+        			horizontalRightInput = Input.GetKey(KeyCode.D);
+        		}else if (PlayerID == 1) {
+        			verticalUpInput = Input.GetKey(KeyCode.UpArrow);
+        			verticalDownInput = Input.GetKey(KeyCode.DownArrow);
+        			horizontalLeftInput = Input.GetKey(KeyCode.LeftArrow);
+        			horizontalRightInput = Input.GetKey(KeyCode.RightArrow);
+        		}
+        		
+        		if (verticalUpInput) {
+        			this.transform.position += new Vector3(0,0,moveSpeed*Time.deltaTime);
+        		}
+        
+        		if (verticalDownInput) {
+        			this.transform.position -= new Vector3(0,0,moveSpeed*Time.deltaTime);
+        		}
+        		if (horizontalLeftInput) {
+        			this.transform.position -= new Vector3(moveSpeed*Time.deltaTime,0,0);
+        		}
+        		if (horizontalRightInput) {
+        			this.transform.position += new Vector3(moveSpeed*Time.deltaTime,0,0);
+        		}
+	}
+	void SwitchMode() {
+		if (PlayerID == 0) {
+			if (Input.GetKeyDown(KeyCode.F)) {
+				modeID = (modeID+1) % modeNum;
+				if (targetFurniture == null) {
+					return;
+				}
+				targetFurniture.GetComponent<Furnitures>().ReleaseScaleLock(PlayerID);
+			}
 		}else if (PlayerID == 1) {
-			verticalUpInput = Input.GetKey(KeyCode.UpArrow);
-			verticalDownInput = Input.GetKey(KeyCode.DownArrow);
-			horizontalLeftInput = Input.GetKey(KeyCode.LeftArrow);
-			horizontalRightInput = Input.GetKey(KeyCode.RightArrow);
+			if (Input.GetKeyDown(KeyCode.P)) {
+				modeID = (modeID+1) % modeNum;
+				if (targetFurniture == null) {
+					return;
+				}
+				targetFurniture.GetComponent<Furnitures>().ReleaseScaleLock(PlayerID);
+			}
 		}
 		
-		if (verticalUpInput) {
-			this.transform.position += new Vector3(0,0,moveSpeed*Time.deltaTime);
+	}
+
+	void ManipulateFurniture() {
+		if (targetFurniture == null) {
+			return;
 		}
 
-		if (verticalDownInput) {
-			this.transform.position -= new Vector3(0,0,moveSpeed*Time.deltaTime);
+		if (PlayerID == 0) {
+			if (Input.GetKey(KeyCode.Q)) {
+				if (0 <= modeID && modeID <= 2) {
+					targetFurniture.GetComponent<Furnitures>().Scale(PlayerID, modeID, manipulateStep);
+				}else if (modeID == 3) {
+					targetFurniture.GetComponent<Furnitures>().ChangeColor(PlayerID,manipulateStep);
+				}
+			}
+
+			if (Input.GetKey(KeyCode.E)) {
+				if (0 <= modeID && modeID <= 2) {
+					targetFurniture.GetComponent<Furnitures>().Scale(PlayerID, modeID, -manipulateStep);
+				}else if (modeID == 3) {
+					targetFurniture.GetComponent<Furnitures>().ChangeColor(PlayerID,-manipulateStep);
+				}
+			}
 		}
-		if (horizontalLeftInput) {
-			this.transform.position -= new Vector3(moveSpeed*Time.deltaTime,0,0);
+		else if (PlayerID == 1) {
+			if (Input.GetKey(KeyCode.K)) {
+				if (0 <= modeID && modeID <= 2) {
+					targetFurniture.GetComponent<Furnitures>().Scale(PlayerID, modeID, manipulateStep);
+				}
+			}
+
+			if (Input.GetKey(KeyCode.L)) {
+				if (0 <= modeID && modeID <= 2) {
+					targetFurniture.GetComponent<Furnitures>().Scale(PlayerID, modeID, -manipulateStep);
+				}
+			}
 		}
-		if (horizontalRightInput) {
-			this.transform.position += new Vector3(moveSpeed*Time.deltaTime,0,0);
+	
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("furniture")) {
+			targetFurniture = other.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("furniture")) {
+			targetFurniture.GetComponent<Furnitures>().ReleaseScaleLock(PlayerID);
+			targetFurniture = null;
 		}
 	}
 }
